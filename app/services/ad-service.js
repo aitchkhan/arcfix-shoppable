cc1ec077e86ae432f8c7c1ed9304739ddaeae3db
vineshhpatel <vineshhpatel@gmail.com>
2015-06-19 03:23:02 +0530

    text color style change on home page, created mock api to get product group

--- app/services/ad-service.js
+++ app/services/ad-service.js
@@ -1,23 +1,44 @@
 angular.module('shopthatvid')
-  .factory('adService',
-    function (GLOBALS, $http) {
-      return {
-        getAds: function () {
-          return $http({method: 'GET', url: GLOBALS.adUrl}).then(function (res) {
-            return res.data;
-          });
-        },
-        getAd:function(id){
-          return $http({method: 'GET', url: GLOBALS.adUrl}).then(function (res) {
-            var data =  _.find(res.data,{ID:parseInt(id)});
-            return data;
-          });
-        },
-        search: function () {
-          return $http({method: 'GET', url: GLOBALS.searchUrl}).then(function (res) {
-            return res.data;
-          });
-        }
-      };
-    })
-  .constant('GLOBALS', {adUrl: 'data/ads.json', searchUrl: 'data/search.json'});
+	.factory('adService', function (GLOBALS, $http) {
+		return {
+			getAds: function () {
+				return $http({
+					method: 'GET', url: GLOBALS.adUrl
+				})
+				.then(function (res) {
+					return res.data;
+				});
+			},
+			getAd:function(id){
+				return $http({
+					method: 'GET', url: GLOBALS.adUrl
+				})
+				.then(function (res) {
+					var data =  _.find(res.data,{ID:parseInt(id)});
+					return data;
+				});
+			},
+			getProductGroup:function(pgId){
+				return $http({
+					method: 'GET', url: GLOBALS.projectGroupUrl 
+				})
+				.then(function (res) {
+					return res.data;
+				});
+			},
+			search: function () {
+				return $http({
+					method: 'GET', url: GLOBALS.searchUrl
+				})
+				.then(function (res) {
+					return res.data;
+				});
+			}
+		};
+	})
+
+.constant('GLOBALS', { 
+	adUrl: 'data/ads.json', 
+	searchUrl: 'data/search.json',
+	projectGroupUrl: 'data/product_group.json'
+});
